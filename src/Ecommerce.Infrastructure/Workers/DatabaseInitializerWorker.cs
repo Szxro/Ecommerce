b9de59a03c713b0c5d5aa7e1819d7847b88da6a2
Szxro <szxro@gmail.com>
2025-02-07 11:46:48 -0400

    Add database iniitliazer worker

---
+++ src/Ecommerce.Infrastructure/Workers/DatabaseInitializerWorker.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Infrastructure.Common;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Ecommerce.Infrastructure.Workers;
+
+public class DatabaseInitializerWorker : BaseWorker<DatabaseInitializerWorker>
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public DatabaseInitializerWorker(
+        ILogger<BaseWorker<DatabaseInitializerWorker>> logger,
+        IServiceScopeFactory scopeFactory) : base(logger)
+    {
+        _scopeFactory = scopeFactory;
+    }
+
+    public override async Task RunAsync(CancellationToken cancellationToken)
+    {
+        using IServiceScope scope = _scopeFactory.CreateScope(); ;
+
+        IDatabaseServiceInitializer initializerService = scope.ServiceProvider.GetRequiredService<IDatabaseServiceInitializer>();
+
+        await initializerService.CanConnectAsync(cancellationToken);
+
+        await initializerService.MigrateAsync(cancellationToken);
+    }
+}
