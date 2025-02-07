7c31a182ef692eeb75560fd2a57e67c3176ccb23
Szxro <szxro@gmail.com>
2025-02-07 12:44:09 -0400

    Add base configuration abstract class

---
+++ src/Ecommerce.Infrastructure/Common/BaseOptions.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.SharedKernel.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Ecommerce.Infrastructure.Common;
+
+public abstract class BaseOptions<TOptions> : IConfigureOptions<TOptions>
+    where TOptions : class, IConfigurationOptions
+{
+    private readonly IConfiguration _configuration;
+
+    protected BaseOptions(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Configure(TOptions options)
+    {
+        _configuration
+            .GetSection(options.SectionName)
+            .Bind(options);
+    }
+}
