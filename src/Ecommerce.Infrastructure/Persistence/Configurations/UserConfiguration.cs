62ee71ce23a2a973d3d25ead23e267a779b0a433
Szxro <szxro@gmail.com>
2025-02-07 11:03:26 -0400

    Add  conifguration folder and user configuration

---
+++ src/Ecommerce.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Persistence.Configurations;
+
+public sealed class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasIndex(x => x.Email).IsUnique();
+
+        builder.HasIndex(x => x.Username).IsUnique();
+    }
+}
