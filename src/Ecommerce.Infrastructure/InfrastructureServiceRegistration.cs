6b53eaef18b907a91d021da562871e5d43fde052
Szxro <szxro@gmail.com>
2025-02-07 12:48:36 -0400

    Add validators from assembly, inteceptors, workers, options and the db context into DI container

--- src/Ecommerce.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/Ecommerce.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,17 +1,47 @@
 ﻿using Ecommerce.Infrastructure.Extensions;
+using Ecommerce.Infrastructure.Options.Database;
+using Ecommerce.Infrastructure.Persistence;
+using Ecommerce.Infrastructure.Persistence.Interceptors;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Ecommerce.Infrastructure;
 
 public static class InfrastructureServiceRegistration
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IHostEnvironment environment)
     {
+        services.AddValidatorsFromAssembly(typeof(InfrastructureServiceRegistration).Assembly);
 
+        services
+            .AddInterceptors()
+            .AddWorkers()
+            .AddConfigurableOptions();
+
+        services.AddDbContext<AppDbContext>((provider, options) =>
+        {
+            DatabaseOptions databaseOptions = provider.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+
+            options.UseSqlServer(databaseOptions.ConnectionString,options =>
+            {
+                options.CommandTimeout(databaseOptions.CommandTimeout);            
+            })
+            .AddInterceptors(provider.GetRequiredService<AuditableEntityInterceptor>())
+            .UseSnakeCaseNamingConvention();
+
+            if (environment.IsDevelopment())
+            {
+                options.EnableDetailedErrors(databaseOptions.EnableDetailedErrors);
+                options.EnableSensitiveDataLogging(databaseOptions.EnableSensitiveDataLogging);
+            }
+        });
 
         services.RegisterServicesFromAssembly(typeof(InfrastructureServiceRegistration).Assembly);
-
-        services.AddWorkers();
 
         return services;
     }
