2af633e678cb8604d74b6e7b77f759830cd2839f
Szxro <szxro@gmail.com>
2025-02-07 10:58:56 -0400

    Add inject attribute and extension method for assembly scanning

---
+++ src/Ecommerce.Infrastructure/Attributes/InjectAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class InjectAttribute : Attribute
+{
+    public InjectAttribute(ServiceLifetime serviceLifetime)
+    {
+        ServiceLifetime = ServiceLifetime;
+    }
+    public ServiceLifetime ServiceLifetime { get; set; }
+}
---
+++ src/Ecommerce.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Ecommerce.Infrastructure.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Ecommerce.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    /// <summary>
+    /// Scans the specified assembly for classes marked with the <see cref="InjectAttribute"/> 
+    /// and registers them in the service collection according to the lifetime defined in the attribute.
+    /// If a class implements multiple interfaces, only the first one found is registered.
+    /// If no interface is implemented, the class is not registered.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> where the discovered services will be registered.</param>
+    /// <param name="assembly">The <see cref="Assembly"/> to scan for injectable services.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/> instance containing the registered services.</returns>
+    /// <exception cref="ArgumentException">Thrown when an invalid service lifetime is encountered.</exception>
+    public static IServiceCollection RegisterServicesFromAssembly(this IServiceCollection services, Assembly assembly)
+    {
+        IEnumerable<TypeInfo> types = assembly.DefinedTypes
+                                              .Where(x => x.GetCustomAttribute<InjectAttribute>() is not null && !x.IsInterface && !x.IsAbstract && x.IsClass);
+
+        foreach (TypeInfo type in types)
+        {
+            InjectAttribute? attribute = type.GetCustomAttribute<InjectAttribute>();
+
+            Type? interfaceType = type.GetInterfaces().FirstOrDefault();
+
+            if (attribute is null || interfaceType is null) continue;
+
+            switch (attribute.ServiceLifetime)
+            {
+                case ServiceLifetime.Scoped:
+                    services.AddScoped(interfaceType, type);
+                    break;
+                case ServiceLifetime.Singleton:
+                    services.AddSingleton(interfaceType, type);
+                    break;
+                case ServiceLifetime.Transient:
+                    services.AddTransient(interfaceType, type);
+                    break;
+                default:
+                    throw new ArgumentException("Unknown Service Lifetime {serviceLifeTime}", nameof(attribute.ServiceLifetime));
+            }
+        }
+
+        return services;
+    }
+}
