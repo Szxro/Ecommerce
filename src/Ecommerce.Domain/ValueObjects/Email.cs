1016222c24c72e310a0befd290ab186863b9222f
Szxro <szxro@gmail.com>
2025-02-07 15:45:18 -0400

    Add email and username value object

---
+++ src/Ecommerce.Domain/ValueObjects/Email.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.SharedKernel.Common.Primitives;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+namespace Ecommerce.Domain.ValueObjects;
+
+public sealed class Email
+{
+    public const string EmailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+    public string Value { get; }
+
+    [JsonConstructor]
+    private Email(string value)
+    {
+        Value = value;
+    }
+
+    public static bool IsValid(string email, out Result<Email>? validEmail)
+    {
+        validEmail = null;
+
+        if (string.IsNullOrEmpty(email) || string.IsNullOrWhiteSpace(email))
+        {
+            return false;
+        }
+
+        if (!Regex.IsMatch(email, EmailPattern, RegexOptions.CultureInvariant))
+        {
+            return false;
+        }
+
+        validEmail = Result<Email>.Success(new Email(email));
+        return true;
+    }
+}
---
+++ src/Ecommerce.Domain/ValueObjects/Username.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.SharedKernel.Common.Primitives;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+namespace Ecommerce.Domain.ValueObjects;
+
+public sealed class Username
+{
+    public const string UsernamePattern = @"^[a-zA-Z0-9_]+$";
+
+    public string Value { get; }
+
+    [JsonConstructor]
+    private Username(string value)
+    {
+        Value = value;
+    }
+
+    public static bool IsValid(string username, out Result<Username>? validUsername)
+    {
+        validUsername = null;
+
+        if (string.IsNullOrEmpty(username) || string.IsNullOrWhiteSpace(username))
+        {
+            return false;
+        }
+
+        if (!Regex.IsMatch(username, UsernamePattern, RegexOptions.CultureInvariant))
+        {
+            return false;
+        }
+
+        validUsername = Result<Username>.Success(new Username(username));
+        return true;
+    }
+}
