be6deac25e1d32a3b8396697a85e596f985d97d0
Szxro <szxro@gmail.com>
2025-02-07 11:49:04 -0400

    Add the services scan and workers into the DI

--- src/Ecommerce.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/Ecommerce.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,18 @@
-﻿namespace Ecommerce.Infrastructure;
+﻿using Ecommerce.Infrastructure.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure;
 
 public static class InfrastructureServiceRegistration
 {
-    
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+
+
+        services.RegisterServicesFromAssembly(typeof(InfrastructureServiceRegistration).Assembly);
+
+        services.AddWorkers();
+
+        return services;
+    }
 }
