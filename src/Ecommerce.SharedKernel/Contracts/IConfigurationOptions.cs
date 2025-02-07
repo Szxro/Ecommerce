90282b4febfe3f8b230b9716b108d893220c9323
Szxro <szxro@gmail.com>
2025-02-07 12:45:55 -0400

    Add marker interface for the options

---
+++ src/Ecommerce.SharedKernel/Contracts/IConfigurationOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.SharedKernel.Contracts;
+
+/// <summary>
+/// Marker interface for the options classes
+/// </summary>
+public interface IConfigurationOptions
+{
+    public string SectionName { get; }
+}
