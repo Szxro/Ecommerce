601d291e58594ef7b423ffb9bf0983e65840e09d
Szxro <szxro@gmail.com>
2025-02-07 10:20:04 -0400

    Add domain layer , entities folder and base user entity

---
+++ src/Ecommerce.Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.SharedKernel.Common;
+
+namespace Ecommerce.Domain.Entities;
+
+public class User : Entity
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    // Value Objects?
+    public string Username { get; set; } = string.Empty; // Unique!
+
+    public string Email { get; set; } = string.Empty; // Unique! 
+
+    public bool IsEmailVerified { get; set; }
+
+    public bool IsAdmin { get; set; }
+
+    public bool LockOutEnabled { get; set; } = true;
+
+    public DateTimeOffset LockOutEndAtUtc { get; set; } = new DateTimeOffset(1999,01,01,00,00,00,TimeSpan.Zero);
+
+    public int AccessFailedCount { get; set; }
+}
