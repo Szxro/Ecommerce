98cef955d7260dc2d78ed0141f9388de8c322001
Szxro <szxro@gmail.com>
2025-02-07 12:46:25 -0400

    Add interceptor extensions method

---
+++ src/Ecommerce.Infrastructure/Extensions/InterceptorsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Infrastructure.Persistence.Interceptors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddInterceptors(this IServiceCollection services)
+    {
+        services.AddSingleton<AuditableEntityInterceptor>();
+
+        return services;
+    }
+}
