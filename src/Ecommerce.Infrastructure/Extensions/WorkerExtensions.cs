20b17cd540e3f679b3e0fbb10ade86bde38bd6e6
Szxro <szxro@gmail.com>
2025-02-07 11:47:16 -0400

    Add worker extension

---
+++ src/Ecommerce.Infrastructure/Extensions/WorkerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Infrastructure.Workers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddWorkers(this IServiceCollection services)
+    {
+        services.AddHostedService<DatabaseInitializerWorker>();
+
+        return services;
+    }
+}
