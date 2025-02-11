c7234834594856347d787a2a52bc99208a851c4b
Szxro <szxro@gmail.com>
2025-02-11 16:18:41 -0400

    Add expired strategy interface

---
+++ src/Ecommerce.Domain/Contracts/IExpiredStrategy.cs
@@ -0,0 +1,11 @@
+﻿using Ecommerce.SharedKernel.Common;
+
+namespace Ecommerce.Domain.Contracts;
+
+public interface IExpiredStrategy<TEntity>
+    where TEntity : Entity
+{
+    Task<List<TEntity>> GetExpiredEntitiesAsync(DateTime currentDateTime, CancellationToken cancellationToken = default);
+
+    void MarkEntitiesAsExpired(List<TEntity> entities);
+}
