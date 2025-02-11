4d232beae12099bcad7b73958a92a636fe97bb72
Szxro <szxro@gmail.com>
2025-02-11 16:19:29 -0400

    Add strategies for email code and refresh toke expiration

---
+++ src/Ecommerce.Infrastructure/Strategies/EmailCodeExpirationStrategy.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Infrastructure.Strategies;
+
+public class EmailCodeExpirationStrategy : IExpiredStrategy<EmailCode>
+{
+    public EmailCodeExpirationStrategy()
+    {
+        
+    }
+
+    public Task<List<EmailCode>> GetExpiredEntitiesAsync(
+        DateTime currentDateTime,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MarkEntitiesAsExpired(List<EmailCode> entities)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Ecommerce.Infrastructure/Strategies/RefreshTokenExpirationStrategy.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Infrastructure.Strategies;
+
+public class RefreshTokenExpirationStrategy : IExpiredStrategy<RefreshToken>
+{
+    public RefreshTokenExpirationStrategy()
+    {
+        
+    }
+
+    public Task<List<RefreshToken>> GetExpiredEntitiesAsync(
+        DateTime currentDateTime,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MarkEntitiesAsExpired(List<RefreshToken> entities)
+    {
+        throw new NotImplementedException();
+    }
+}
