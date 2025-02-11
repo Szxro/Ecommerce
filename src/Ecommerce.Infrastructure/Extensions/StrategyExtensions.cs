5cef3b9de346d1facf2f4c2ef1bcfcfb8ac98afe
Szxro <szxro@gmail.com>
2025-02-11 16:21:34 -0400

    Add strategy extension method to add the strategies to the DI

---
+++ src/Ecommerce.Infrastructure/Extensions/StrategyExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Infrastructure.Services;
+using Ecommerce.Infrastructure.Strategies;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Extensions;
+
+public static partial class InfrastructureExtensions
+{
+    public static IServiceCollection AddStrategies(this IServiceCollection services)
+    {
+        // Context
+        services.AddScoped<ExpirationStrategyService<EmailCode>>();
+
+        services.AddScoped<ExpirationStrategyService<RefreshToken>>();
+
+        // Strategies
+        services.AddScoped<IExpiredStrategy<EmailCode>, EmailCodeExpirationStrategy>();
+
+        services.AddScoped<IExpiredStrategy<RefreshToken>, RefreshTokenExpirationStrategy>();
+
+        return services;
+    }
+}
