b6979a142e39c929bbe1b2748aa29cc2e57be9ac
Szxro <szxro@gmail.com>
2025-02-12 12:46:58 -0400

    Add initial migrations

---
+++ src/Ecommerce.Infrastructure/Persistence/Migrations/20250212163531_Init.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    is_email_verified = table.Column<bool>(type: "bit", nullable: false),
+                    is_admin = table.Column<bool>(type: "bit", nullable: false),
+                    lock_out_enabled = table.Column<bool>(type: "bit", nullable: false),
+                    lock_out_end_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    access_failed_count = table.Column<int>(type: "int", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    is_deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deleted_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "credentials",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    hash_value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    salt_value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    is_active = table.Column<bool>(type: "bit", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    is_deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deleted_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_credentials", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_credentials_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "email_code",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    is_expired = table.Column<bool>(type: "bit", nullable: false),
+                    is_revoked = table.Column<bool>(type: "bit", nullable: false),
+                    is_used = table.Column<bool>(type: "bit", nullable: false),
+                    expiration_date_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    is_deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deleted_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_email_code", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_email_code_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "refresh_token",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    is_expired = table.Column<bool>(type: "bit", nullable: false),
+                    is_revoked = table.Column<bool>(type: "bit", nullable: false),
+                    is_used = table.Column<bool>(type: "bit", nullable: false),
+                    expiration_date_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    is_deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deleted_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_refresh_token", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_refresh_token_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_credentials_user_id",
+                table: "credentials",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_email_code_user_id",
+                table: "email_code",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_refresh_token_user_id",
+                table: "refresh_token",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_email_value",
+                table: "user",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_username_value",
+                table: "user",
+                column: "username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "credentials");
+
+            migrationBuilder.DropTable(
+                name: "email_code");
+
+            migrationBuilder.DropTable(
+                name: "refresh_token");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
