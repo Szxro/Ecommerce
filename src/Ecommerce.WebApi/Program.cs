3d36cc4eae19decf7d87654309ca4a68d7cb936f
Szxro <szxro@gmail.com>
2025-02-12 11:59:40 -0400

    Add application and infrastructure layer into the DI

--- src/Ecommerce.WebApi/Program.cs
+++ src/Ecommerce.WebApi/Program.cs
@@ -1,7 +1,13 @@
+using Ecommerce.Infrastructure;
+using Ecommerce.Application;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-{ 
+{
 
     // Add services to the container.
+    builder.Services
+           .AddInfrastructure(builder.Environment)
+           .AddApplication();
 
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
