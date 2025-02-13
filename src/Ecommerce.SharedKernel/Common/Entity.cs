650b634dd715420bbd488d840df10a88e3cb12b0
Szxro <szxro@gmail.com>
2025-02-13 11:58:38 -0400

    Add domain events to the entity class

--- src/Ecommerce.SharedKernel/Common/Entity.cs
+++ src/Ecommerce.SharedKernel/Common/Entity.cs
@@ -1,4 +1,7 @@
-﻿namespace Ecommerce.SharedKernel.Common;
+﻿using Ecommerce.SharedKernel.Contracts;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.SharedKernel.Common;
 
 public abstract class Entity : AuditableEntity
 {
@@ -6,5 +9,25 @@ public abstract class Entity : AuditableEntity
 
     public bool IsDeleted { get; set; }
 
-    public DateTime DeletedAtUtc { get; set; }
+    public DateTime DeletedAtUtc { get; set; } = DateTime.MinValue;
+
+    private List<IDomainEvent> domainEvents = new List<IDomainEvent>();
+
+    [NotMapped]
+    public IReadOnlyCollection<IDomainEvent> DomainEvent => domainEvents;
+
+    public void AddEvent(IDomainEvent @event)
+    {
+        domainEvents.Add(@event);
+    }
+
+    public void RemoveEvent(IDomainEvent @event)
+    {
+        domainEvents.Remove(@event);
+    }
+
+    public void ClearEvents()
+    {
+        domainEvents.Clear();
+    }
 }
