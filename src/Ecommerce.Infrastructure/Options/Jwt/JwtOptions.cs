6ab7e3cd286f24b7103b79b14501a0b5af02d1c3
Szxro <szxro@gmail.com>
2025-02-13 11:22:57 -0400

    Add jwt options, jwt bearer token options and validations

---
+++ src/Ecommerce.Infrastructure/Options/Jwt/JwtOptions.cs
@@ -0,0 +1,27 @@
+﻿using Ecommerce.Infrastructure.Common;
+using Ecommerce.SharedKernel.Contracts;
+using Microsoft.Extensions.Configuration;
+
+namespace Ecommerce.Infrastructure.Options.Jwt;
+
+public class JwtOptionsSetup : BaseOptions<JwtOptions>
+{
+    public JwtOptionsSetup(IConfiguration configuration) : base(configuration) { }
+}
+
+public class JwtOptions : IConfigurationOptions
+{
+    public string SectionName => "JwtOptions";
+
+    public bool ValidateAudience { get; set; }
+
+    public bool ValidateIssuer { get; set; }
+
+    public bool ValidateLifetime { get; set; }
+
+    public string ValidIssuer { get; set; } = string.Empty;
+
+    public string ValidAudience { get; set; } = string.Empty;
+
+    public string SecretKey { get; set; } = string.Empty;
+}
