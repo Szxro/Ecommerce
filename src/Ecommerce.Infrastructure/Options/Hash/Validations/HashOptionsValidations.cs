ee909049d799442be8ae8b446c03ce9653af5902
Szxro <szxro@gmail.com>
2025-02-13 11:55:27 -0400

    Add hash options and validations

---
+++ src/Ecommerce.Infrastructure/Options/Hash/Validations/HashOptionsValidations.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Ecommerce.Infrastructure.Options.Hash.Validations;
+
+public class HashOptionsValidations : AbstractValidator<HashOptions>
+{
+    public HashOptionsValidations()
+    {
+        RuleFor(x => x.Iterations)
+           .NotEmpty().WithMessage("The {PropertyName} can't empty")
+           .NotNull().WithMessage("The {PropertyName} can't null")
+           .GreaterThan(10000).WithMessage("The {PropertyName} must be greater than 10000");
+
+        RuleFor(x => x.SaltSize)
+            .NotEmpty().WithMessage("The {PropertyName} can't empty")
+            .NotNull().WithMessage("The {PropertyName} can't null")
+            .GreaterThan(0).WithMessage("The {PropertyName} must be greater than 0");
+
+        RuleFor(x => x.HashSize)
+            .NotEmpty().WithMessage("The {PropertyName} can't empty")
+            .NotNull().WithMessage("The {PropertyName} can't null")
+            .GreaterThan(0).WithMessage("The {PropertyName} must be greater than 0");
+    }
+}
