86dfaca2739fed2b4d6e144bd59754f3405ce485
Szxro <szxro@gmail.com>
2025-02-13 11:25:38 -0400

    Change how the options and setup are split

--- src/Ecommerce.Infrastructure/Options/Database/DatabaseOptions.cs
+++ src/Ecommerce.Infrastructure/Options/Database/DatabaseOptions.cs
@@ -1,6 +1,13 @@
-﻿using Ecommerce.SharedKernel.Contracts;
+﻿using Ecommerce.Infrastructure.Common;
+using Ecommerce.SharedKernel.Contracts;
+using Microsoft.Extensions.Configuration;
 
 namespace Ecommerce.Infrastructure.Options.Database;
+
+public sealed class DatabaseOptionsSetup : BaseOptions<DatabaseOptions>
+{
+    public DatabaseOptionsSetup(IConfiguration configuration) : base(configuration) { }
+}
 
 public sealed class DatabaseOptions : IConfigurationOptions
 {
