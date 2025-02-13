02afc7d0433dab7677036c928550f4c0f3099c3a
Szxro <szxro@gmail.com>
2025-02-13 11:55:00 -0400

    Add hash service implementation and interface

---
+++ src/Ecommerce.Domain/Contracts/IHashService.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Domain.Contracts;
+
+public interface IHashService
+{
+    (string hash, byte[] salt) GetHashAndSalt(string password);
+
+    bool VerifyHash(string password, string currentHash, string currentSalt);
+}
