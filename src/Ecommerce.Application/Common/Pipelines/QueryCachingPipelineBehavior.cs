c0263ea89c36d3d741a992dc584398c9ebbb4664
Szxro <szxro@gmail.com>
2025-02-14 09:50:38 -0400

    Add query caching pipeline

---
+++ src/Ecommerce.Application/Common/Pipelines/QueryCachingPipelineBehavior.cs
@@ -0,0 +1,49 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.SharedKernel.Contracts;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ecommerce.Application.Common.Pipelines;
+
+public class QueryCachingPipelineBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICachedQuery
+    where TResponse : Result
+{
+    private readonly ICacheService _cacheService;
+    private readonly ILogger<QueryCachingPipelineBehavior<TRequest, TResponse>> _logger;
+
+    public QueryCachingPipelineBehavior(
+        ICacheService cacheService,
+        ILogger<QueryCachingPipelineBehavior<TRequest, TResponse>> logger)
+    {
+        _cacheService = cacheService;
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        TResponse? cachedResult = _cacheService.Get<TResponse>(request.CachedKey);
+
+        string requestName = typeof(TRequest).Name;
+
+        if (cachedResult is not null)
+        {
+            _logger.LogInformation("Cache hit for the request {requestName}", requestName);
+
+            return cachedResult;
+        }
+
+        _logger.LogInformation("Cache miss for the request {requestName}", requestName);
+
+        TResponse response = await next();
+
+        if (response.IsSuccess)
+        {
+            _cacheService.Set(request.CachedKey, response,request.Options);
+        }
+
+        return response;
+    }
+}
