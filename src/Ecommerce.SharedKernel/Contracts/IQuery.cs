69f42c940979cf4179d90a98915fe92e16102810
Szxro <szxro@gmail.com>
2025-02-14 09:48:56 -0400

    Add cached query interface

--- src/Ecommerce.SharedKernel/Contracts/IQuery.cs
+++ src/Ecommerce.SharedKernel/Contracts/IQuery.cs
@@ -1,5 +1,15 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Ecommerce.SharedKernel.Contracts;
 
 public interface IQuery<out TResponse> : IRequest<TResponse> { }
+
+public interface ICachedQuery<out TResponse> : IQuery<TResponse>, ICachedQuery { }
+
+public interface ICachedQuery
+{
+    string CachedKey { get; }
+
+    MemoryCacheEntryOptions? Options { get; }
+}
