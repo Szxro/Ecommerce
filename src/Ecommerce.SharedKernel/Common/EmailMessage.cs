f881558468cd1cfa2e2a2db151bbd19679426552
Szxro <szxro@gmail.com>
2025-02-14 11:06:11 -0400

    Add email message common entity

---
+++ src/Ecommerce.SharedKernel/Common/EmailMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.SharedKernel.Common;
+
+public class EmailMessage
+{
+    public string ToAddress { get; set; } = string.Empty;
+
+    public string Subject { get; set; } = string.Empty;
+
+    public string Body { get; set; } = string.Empty;
+}
