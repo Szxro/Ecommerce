9eb48890c515f36519edc6aa2903a7e3bf3b0d95
Szxro <szxro@gmail.com>
2025-02-18 15:59:58 -0400

    Add token service unit tests and global using to it

--- tests/Ecommerce.Infrastructure.UnitTests/GlobalUsing.cs
+++ tests/Ecommerce.Infrastructure.UnitTests/GlobalUsing.cs
@@ -1,2 +1,5 @@
 ﻿global using Ecommerce.Infrastructure.Services;
 global using Ecommerce.Infrastructure.Options.Hash;
+global using Microsoft.Extensions.Logging;
+global using NSubstitute;
+global using Ecommerce.Infrastructure.Options.Jwt;
---
+++ tests/Ecommerce.Infrastructure.UnitTests/Services/TokenServiceTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Ecommerce.Infrastructure.UnitTests.Services;
+
+using Options = Microsoft.Extensions.Options.Options;
+
+public class TokenServiceTests
+{
+    [Fact]
+    public void GenerateRefreshToken_ValidRefreshToken_ReturnUniqueRefreshToken()
+    {
+        // Arrange
+        TokenService tokenService = MakeTokenService();
+
+        // Act
+        string refreshToken1 = tokenService.GenerateRefreshToken();
+
+        string refreshToken2 = tokenService.GenerateRefreshToken();
+
+        // Assert
+        Assert.NotEqual(refreshToken1,refreshToken2);
+    }
+
+    private static TokenService MakeTokenService()
+    {
+        return new TokenService(
+            Substitute.For<ILogger<TokenService>>(),
+            Options.Create(
+                new JwtOptions()
+            {
+                SecretKey = "mysecretkey",
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidAudience = "audience",
+                ValidIssuer = "issuer"
+            }));
+    }
+}
