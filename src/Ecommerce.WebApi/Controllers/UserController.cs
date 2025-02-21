5771a14617322c802a1651f5595ee7949599aa54
Szxro <szxro@gmail.com>
2025-02-21 16:54:04 -0400

    Add user controller

---
+++ src/Ecommerce.WebApi/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.Application.Features.Users.Commands.CreateUserCommand;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.WebApi.Common;
+using Ecommerce.WebApi.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebApi.Controllers;
+
+[Route("api/user")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public UserController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [HttpPost("register-user")]
+    public async Task<IResult> RegisterNewUser(CreateUserCommand userCommand)
+    {
+        Result result = await _sender.Send(userCommand);
+
+        return result.Match(
+            onSuccess: Results.Created,
+            onFailure: CustomResult.Problem);
+    }
+}
