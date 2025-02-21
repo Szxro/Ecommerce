8d1cc5b084aed3fa5fcc162af16640fdf44c45eb
Szxro <szxro@gmail.com>
2025-02-21 16:44:47 -0400

    Add user errprs

---
+++ src/Ecommerce.Domain/Errors/UserErrors.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.SharedKernel.Common.Primitives;
+
+namespace Ecommerce.Domain.Errors;
+
+public static class UserErrors
+{
+    public static Error EmailNotUniqueOrInvalid
+            => Error.Validation("Email is not valid or is already registered.");
+
+    public static Error UsernameNotUniqueOrInvalid
+            => Error.Validation("Username is not valid or is already registered.");
+}
