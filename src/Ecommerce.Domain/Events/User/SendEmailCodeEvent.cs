e8e14f4edf1f28b8873e633d49c89a73ca4647b2
Szxro <szxro@gmail.com>
2025-02-21 16:44:20 -0400

    Add user event

---
+++ src/Ecommerce.Domain/Events/User/SendEmailCodeEvent.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.Domain.ValueObjects;
+using Ecommerce.SharedKernel.Contracts;
+
+namespace Ecommerce.Domain.Events.User;
+
+public class SendEmailCodeEvent : IDomainEvent
+{
+    public SendEmailCodeEvent(
+        Username username,
+        string firstName,
+        string lastName,
+        Email email,
+        string emailCode)
+    {
+        Username = username;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        EmailCode = emailCode;
+    }
+
+    public string Category => "email";
+
+    public Username Username { get; }
+
+    public string FirstName { get; }
+
+    public string LastName { get; }
+
+    public Email Email { get; }
+
+    public string EmailCode { get; }
+};
