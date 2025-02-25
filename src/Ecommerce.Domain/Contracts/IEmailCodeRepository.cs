f7d6bdeab11bb3f8530156d14210fb6eb8b076eb
Szxro <szxro@gmail.com>
2025-02-25 15:21:08 -0400

    Add email code repository implementation and interface

---
+++ src/Ecommerce.Domain/Contracts/IEmailCodeRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Domain.Contracts;
+
+public interface IEmailCodeRepository : IRepositoryWriter<EmailCode>
+{
+    Task<EmailCode?> GetEmailCodeByEmailCode(string emailCode, CancellationToken cancellationToken = default);
+}
---
+++ src/Ecommerce.Infrastructure/Repositories/EmailCodeRepository.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Infrastructure.Attributes;
+using Ecommerce.Infrastructure.Common;
+using Ecommerce.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Repositories;
+
+[Inject(ServiceLifetime.Scoped)]
+public class EmailCodeRepository : GenericRepository<EmailCode>, IEmailCodeRepository
+{
+    public EmailCodeRepository(AppDbContext appDbContext) : base(appDbContext) { }
+
+    public async Task<EmailCode?> GetEmailCodeByEmailCode(string emailCode,CancellationToken cancellationToken = default)
+    {
+        return await _appDbContext.EmailCode.FirstOrDefaultAsync(x => x.Code == emailCode,cancellationToken);
+    }
+}
