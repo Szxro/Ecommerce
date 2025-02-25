4f18ee5ccdf3a325907de19cda8fa150f06d0a30
Szxro <szxro@gmail.com>
2025-02-25 16:35:00 -0400

    Add email controller

---
+++ src/Ecommerce.WebApi/Controllers/EmailController.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.Application.Features.EmailCodes.Commands.VerifyEmailCode;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.WebApi.Common;
+using Ecommerce.WebApi.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebApi.Controllers;
+
+[Route("api/email")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public EmailController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet("verify-email-code")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IResult> VerifyEmailCode([FromQuery] string emailCode)
+    {
+        Result result = await _sender.Send(new VerifyEmailCodeCommand(emailCode));
+
+        return result.Match(
+            onSuccess: () => Results.Ok(),
+            onFailure: CustomResult.Problem);
+    }
+}
