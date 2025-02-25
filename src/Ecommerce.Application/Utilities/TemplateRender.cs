b576eb96fed9d426b98710deb31a494d91bf9c1e
Szxro <szxro@gmail.com>
2025-02-25 09:51:17 -0400

    Add utilities folder, template render exception and template render utility class

---
+++ src/Ecommerce.Application/Utilities/TemplateRender.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Application.Common.Exceptions;
+using Fluid;
+
+namespace Ecommerce.Application.Utilities;
+
+public class TemplateRender
+{
+    private static readonly FluidParser _parser = new FluidParser();
+
+    public static string Render(string template,object model)
+    {
+        if (_parser.TryParse(template, out IFluidTemplate fluidTemplate, out string error))
+        {
+            TemplateContext context = new TemplateContext(model);
+
+            string render = fluidTemplate.Render(context);
+
+            return render;
+        }
+
+        throw new TemplateRenderException(error);
+    }
+}
