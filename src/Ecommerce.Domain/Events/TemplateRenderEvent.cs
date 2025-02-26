6b8e02a6d72bc737582368cf25c181e14758b2d6
Sebastian <svargas@isf.do>
2025-02-26 14:29:04 -0400

    Add TemplateRenderEvent and remove others unnecessary events

---
+++ src/Ecommerce.Domain/Events/TemplateRenderEvent.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Domain.ValueObjects;
+using Ecommerce.SharedKernel.Contracts;
+using Ecommerce.SharedKernel.Enums;
+
+namespace Ecommerce.Domain.Events;
+
+public class TemplateRenderEvent : IDomainEvent
+{
+    public TemplateRenderEvent(
+        TemplateCategory category,
+        object model,
+        Email email)
+    {
+        Category = category;
+        Model = model;
+        Email = email;
+    }
+
+    public TemplateCategory Category { get; }
+
+    public object Model { get; }
+
+    public Email Email { get; set; }
+}
