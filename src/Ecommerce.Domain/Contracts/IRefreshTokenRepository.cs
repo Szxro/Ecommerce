dcc03f93b2c8752382414f53b1f0a5b3301c209f
Szxro <szxro@gmail.com>
2025-02-27 16:04:54 -0400

    Add refresh token repository implementation and interface

---
+++ src/Ecommerce.Domain/Contracts/IRefreshTokenRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Domain.Contracts;
+
+public interface IRefreshTokenRepository : IRepositoryWriter<RefreshToken>
+{
+    Task<RefreshToken?> GetUnusedUserRefreshTokenByUsernameAsync(string username, CancellationToken cancellationToken = default);
+}
---
+++ src/Ecommerce.Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Infrastructure.Attributes;
+using Ecommerce.Infrastructure.Common;
+using Ecommerce.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Repositories;
+
+[Inject(ServiceLifetime.Scoped)]
+public class RefreshTokenRepository 
+    : GenericRepository<RefreshToken>, IRefreshTokenRepository
+{
+    public RefreshTokenRepository(AppDbContext appDbContext) : base(appDbContext) { }
+
+    public async Task<RefreshToken?> GetUnusedUserRefreshTokenByUsernameAsync(string username, CancellationToken cancellationToken = default)
+    {
+        return await _appDbContext.RefreshToken
+                                  .Where(x => x.User.Username.Value == username)
+                                  .FirstOrDefaultAsync(cancellationToken);
+    }
+}
