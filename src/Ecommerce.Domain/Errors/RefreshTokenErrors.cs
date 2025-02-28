8a955a38b95515c326283814796d677f8f5937ec
Szxro <szxro@gmail.com>
2025-02-28 12:15:48 -0400

    Add token errors and refresh token errors

---
+++ src/Ecommerce.Domain/Errors/RefreshTokenErrors.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.SharedKernel.Common.Primitives;
+
+namespace Ecommerce.Domain.Errors;
+
+public static class RefreshTokenErrors
+{
+    public static Error InvalidRefreshToken
+        => Error.Validation("The provided refresh token is expired or used.");
+}
---
+++ src/Ecommerce.Domain/Errors/TokenErrors.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce.SharedKernel.Common.Primitives;
+
+namespace Ecommerce.Domain.Errors;
+
+public static class TokenErrors
+{
+    public static Error InvalidToken
+        => Error.Validation("The provided token is invalid.");
+
+    public static Error InvalidTokenClaims
+        => Error.Validation( "Token does not contain valid claims");
+
+    public static Error InvalidTokenExpiryStamp
+        => Error.Validation("Token expiration timestamp is invalid.");
+
+    public static Error TokenStillValid
+        => Error.Validation("The provided token is still valid.");
+}
