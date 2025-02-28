37d2ac3111eb815ce0663c29217893c4e9dda660
Szxro <szxro@gmail.com>
2025-02-28 14:23:25 -0400

    Add presentation extensions and swagger gen auth extensions

---
+++ src/Ecommerce.WebApi/Extensions/PresentationExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.WebApi.ExceptionHandler;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace Ecommerce.WebApi.Extensions;
+
+public static partial class ApiExtensions
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services, IHostEnvironment environment)
+    {
+        // Authorization and Authorization
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+        services.AddAuthorization();
+
+        // Core
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGenWithAuth();
+        services.AddProblemDetails();
+
+        // Exception Handlers
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+
+        // Cors
+        services.AddCors(options =>
+        {
+            options.AddPolicy("default", policy =>
+            {
+                policy.SetIsOriginAllowed(origin => true);
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();                                
+            });
+        });
+
+        return services;
+    }
+}
