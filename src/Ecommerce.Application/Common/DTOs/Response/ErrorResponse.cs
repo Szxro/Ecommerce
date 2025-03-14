1ce1d6dd3eb3a4b94207a245156f9a1bcbe38605
Szxro <szxro@gmail.com>
2025-03-14 10:05:00 -0400

    Remove ValidationError from shared kernel and move it to the application layer with some changes to how the errors are show to the front and add error response dto

---
+++ src/Ecommerce.Application/Common/DTOs/Response/ErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce.Application.Common.DTOs.Response;
+
+public class ErrorResponse
+{
+    public ErrorResponse(string errorCode, string description)
+    {
+        ErrorCode = errorCode;
+        Description = description;
+    }
+
+    public string ErrorCode { get; }
+
+    public string Description { get; }
+}
---
+++ src/Ecommerce.Application/Common/Errors/ValidationError.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.Application.Common.DTOs.Response;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.SharedKernel.Enums;
+using FluentValidation.Results;
+
+namespace Ecommerce.Application.Common.Errors;
+
+public class ValidationError : Error
+{
+    public Dictionary<string, List<ErrorResponse>> Errors { get; }
+
+    public ValidationError(ValidationFailure[] failures)
+     : base("Validation.Error",
+            "One or more validation errors occurred.",
+            ErrorType.Validation)
+    {
+        Errors = GroupFailuresByPropertyName(failures);
+    }
+
+    private Dictionary<string, List<ErrorResponse>> GroupFailuresByPropertyName(ValidationFailure[] failures)
+    {
+        return failures
+            .GroupBy(property => property.PropertyName)
+            .ToDictionary(
+                property => property.Key,
+                property => 
+                    property.Select(x => new ErrorResponse(x.ErrorCode, x.ErrorMessage)).ToList());
+    }
+}
