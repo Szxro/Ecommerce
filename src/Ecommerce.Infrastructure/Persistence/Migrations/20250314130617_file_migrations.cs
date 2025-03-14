13e0beda693cf41599d2071f3a05f7da3608a44c
Szxro <szxro@gmail.com>
2025-03-14 10:03:21 -0400

    Add missing image migrations

---
+++ src/Ecommerce.Infrastructure/Persistence/Migrations/20250314130617_file_migrations.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class file_migrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "image",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    file_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    path = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    total_size = table.Column<int>(type: "int", nullable: false),
+                    format = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    height = table.Column<int>(type: "int", nullable: false),
+                    width = table.Column<int>(type: "int", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    is_deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deleted_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_image", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_image",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    image_id = table.Column<int>(type: "int", nullable: false),
+                    is_active = table.Column<bool>(type: "bit", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_image", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_image_image_image_id",
+                        column: x => x.image_id,
+                        principalTable: "image",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_user_image_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_image_image_id",
+                table: "user_image",
+                column: "image_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_image_user_id",
+                table: "user_image",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_image");
+
+            migrationBuilder.DropTable(
+                name: "image");
+        }
+    }
+}
