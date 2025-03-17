e6771f28589fe787353f94e3b09e1b35cc74630e
Szxro <szxro@gmail.com>
2025-03-17 16:03:04 -0400

    Add guard for authentication

---
+++ src/Ecommerce.WebApp/src/app/core/guards/is-authenticated.guard.ts
@@ -0,0 +1,16 @@
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export function isAuthenticatedGuard(route: ActivatedRouteSnapshot,state: RouterStateSnapshot){
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  const isAuthenticated = authService.isAuthenticated();
+
+  if(!isAuthenticated){
+      return router.navigateByUrl('/auth/login');
+  }
+
+  return true;
+}
