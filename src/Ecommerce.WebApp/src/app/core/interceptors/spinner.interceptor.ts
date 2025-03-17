10eff60d75179f34eecdfc24268de26797c8d8c7
Szxro <szxro@gmail.com>
2025-03-17 16:03:56 -0400

    Add spinner interceptor

---
+++ src/Ecommerce.WebApp/src/app/core/interceptors/spinner.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { finalize, Observable } from "rxjs";
+import { SpinnerService } from "../services/spinner.service";
+
+export function spinnerInterceptor(req:HttpRequest<unknown>,next:HttpHandlerFn):Observable<HttpEvent<unknown>>{
+  const spinner = inject(SpinnerService);
+
+  // Start the spinner for the request
+  spinner.updateLoadingStatus(req.url,true);
+
+  return next(req).pipe(
+    finalize(() =>{
+      // Stop the spinner for the request even if an error happen
+      spinner.updateLoadingStatus(req.url,false);
+    })
+  );
+}
