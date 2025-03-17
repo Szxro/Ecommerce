b23e7435847420053277c94c2d91c32e57c432b9
Szxro <szxro@gmail.com>
2025-03-17 16:02:45 -0400

    Add BrowserAnimationModule, Shared module and ToastModule and interceptors

--- src/Ecommerce.WebApp/src/app/app.module.ts
+++ src/Ecommerce.WebApp/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { spinnerInterceptor } from './core/interceptors/spinner.interceptor';
+import { tokenInterceptor } from './core/interceptors/token.interceptor';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -11,12 +15,19 @@ import { SharedModule } from './shared/shared.module';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ToastrModule.forRoot({
+      timeOut:3000,
+      preventDuplicates: true
+    }),
   ],
   providers: [
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([ spinnerInterceptor, tokenInterceptor])
+    )
   ],
   bootstrap: [AppComponent]
 })
