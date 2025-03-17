c2d7c8d34e7b1e39141f73bddfa80b6bc2ad0453
Szxro <szxro@gmail.com>
2025-03-17 09:54:46 -0400

    Delete unused property

--- src/Ecommerce.WebApp/src/app/app.component.ts
+++ src/Ecommerce.WebApp/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Ecommerce.WebApp';
-}
+export class AppComponent {}
