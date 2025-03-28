f29f91e4674613c89d697793dd6405635959d061
Szxro <szxro@gmail.com>
2025-03-28 15:53:21 -0400

    Add UserImage implementation and interface

---
+++ src/Ecommerce.Domain/Contracts/IUserImageRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Domain.Contracts;
+
+public interface IUserImageRepository 
+    : IRepositoryWriter<UserImage>
+{
+    Task<UserImage?> GetActiveUserImageByUsernameAsync(string username, CancellationToken cancellationToken = default);
+}
---
+++ src/Ecommerce.Infrastructure/Repositories/UserImageRepository.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Infrastructure.Attributes;
+using Ecommerce.Infrastructure.Common;
+using Ecommerce.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Repositories;
+
+[Inject(ServiceLifetime.Scoped)]
+public class UserImageRepository
+    : GenericRepository<UserImage>, IUserImageRepository
+{
+    public UserImageRepository(AppDbContext appDbContext) : base(appDbContext){}
+
+    public async Task<UserImage?> GetActiveUserImageByUsernameAsync(string username, CancellationToken cancellationToken = default)
+    {
+        return await _appDbContext.UserImage
+                                  .Where(x => x.IsActive && x.User.Username.Value == username)
+                                  .FirstOrDefaultAsync(cancellationToken);
+    }
+}
