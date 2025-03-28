14a8aa72bf1303d198733e1b65ff2d8c31ea3164
Szxro <szxro@gmail.com>
2025-03-28 15:53:57 -0400

    Make some changes into the user controller and add upload image endpoint

--- src/Ecommerce.WebApi/Controllers/UserController.cs
+++ src/Ecommerce.WebApi/Controllers/UserController.cs
@@ -1,15 +1,19 @@
 ﻿using Ecommerce.Application.Features.Users.Commands.RegisterUserCommand;
 using Ecommerce.Application.Features.Users.Commands.LoginUserCommand;
+using Ecommerce.Application.Features.EmailCodes.Commands.ResendEmailCode;
+using Ecommerce.Application.Features.EmailCodes.Commands.VerifyEmailCode;
 using Ecommerce.Application.Common.DTOs.Response;
 using Ecommerce.SharedKernel.Common.Primitives;
 using Ecommerce.WebApi.Common;
 using Ecommerce.WebApi.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Ecommerce.Application.Features.Users.Commands.UploadImageCommand;
 
 namespace Ecommerce.WebApi.Controllers;
 
-[Route("api/user")]
+[Route("api/users")]
 [ApiController]
 public class UserController : ControllerBase
 {
@@ -22,8 +26,8 @@ public class UserController : ControllerBase
 
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [HttpPost("register-user")]
-    public async Task<IResult> RegisterNewUser(RegisterUserCommand userCommand)
+    [HttpPost("register")]
+    public async Task<IResult> RegisterUser(RegisterUserCommand userCommand)
     {
         Result result = await _sender.Send(userCommand);
 
@@ -34,13 +38,50 @@ public class UserController : ControllerBase
 
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [HttpPost("login-user")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpPost("login")]
     public async Task<IResult> LoginUser(LoginUserCommand loginUserCommand)
     {
         Result<TokenResponse> result = await _sender.Send(loginUserCommand);
 
         return result.Match(
             onSuccess: () => CustomResult.Success(result),
+            onFailure: CustomResult.Problem);
+    }
+
+    [HttpGet("verify-email")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IResult> VerifyEmailCode([FromQuery] string emailCode)
+    {
+        Result result = await _sender.Send(new VerifyEmailCodeCommand(emailCode));
+
+        return result.Match(
+            onSuccess: () => Results.Ok(),
+            onFailure: CustomResult.Problem);
+    }
+
+    [HttpPost("{username}/resend-email")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IResult> ResendEmailCode([FromRoute] string username)
+    {
+        Result result = await _sender.Send(new ResendEmailCodeCommand(username));
+
+        return result.Match(
+            onSuccess: Results.NoContent,
+            onFailure: CustomResult.Problem);
+    }
+
+    [Authorize]
+    [HttpPost("upload-image")]
+    public async Task<IResult> UploadImage(IFormFile formFile)
+    {
+        Result result = await _sender.Send(new UploadImageCommand(formFile));
+
+        return result.Match(
+            onSuccess: Results.NoContent,
             onFailure: CustomResult.Problem);
     }
 }
