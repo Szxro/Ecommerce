5dd36a6b56a14c2a5be1c2f58211dc482acaa915
Sebastian <svargas@isf.do>
2025-03-30 17:30:11 -0400

    Add user address interface and repository

---
+++ src/Ecommerce.Domain/Contracts/IUserAddressRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Domain.Contracts;
+
+public interface IUserAddressRepository 
+    : IRepositoryWriter<UserAddress>
+{
+}
---
+++ src/Ecommerce.Infrastructure/Repositories/UserAddressRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Infrastructure.Attributes;
+using Ecommerce.Infrastructure.Common;
+using Ecommerce.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Repositories;
+
+[Inject(ServiceLifetime.Scoped)]
+public class UserAddressRepository
+    : GenericRepository<UserAddress>, IUserAddressRepository
+{
+    public UserAddressRepository(AppDbContext appDbContext) : base(appDbContext) { }
+}
