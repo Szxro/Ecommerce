023467f7a3eb3b19b0271eb9014a27a3bccd9a8b
Sebastian <svargas@isf.do>
2025-03-30 17:29:29 -0400

    Add country interface and implementation

---
+++ src/Ecommerce.Domain/Contracts/ICountryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Domain.Contracts;
+
+public interface ICountryRepository 
+    : IRepositoryWriter<Country>
+{
+    Task<Country?> GetCountryByCountryNameAsync(string countryName, CancellationToken cancellationToken = default);
+}
---
+++ src/Ecommerce.Infrastructure/Repositories/CountryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Infrastructure.Attributes;
+using Ecommerce.Infrastructure.Common;
+using Ecommerce.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Repositories;
+
+[Inject(ServiceLifetime.Scoped)]
+public class CountryRepository
+    : GenericRepository<Country>, ICountryRepository
+{
+    public CountryRepository(AppDbContext appDbContext) : base(appDbContext) { }
+
+    public async Task<Country?> GetCountryByCountryNameAsync(string countryName, CancellationToken cancellationToken = default)
+    {
+        return await _appDbContext.Country.Where(x => x.Name == countryName).FirstOrDefaultAsync(cancellationToken);
+    }
+}
