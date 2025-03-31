60ead4ebb9636d234c78fa5ce2766842626f2f69
Szxro <szxro@gmail.com>
2025-03-31 11:30:08 -0400

    Add address migration with country entity

---
+++ src/Ecommerce.Infrastructure/Persistence/Migrations/20250331152850_Adress.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "country",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    is_deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deleted_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_country", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "address",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    street_number = table.Column<int>(type: "int", nullable: false),
+                    address_line1 = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    address_line2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    city = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    region = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    postal_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    country_id = table.Column<int>(type: "int", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    is_deleted = table.Column<bool>(type: "bit", nullable: false),
+                    deleted_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_address", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_address_country_country_id",
+                        column: x => x.country_id,
+                        principalTable: "country",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_address",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    address_id = table.Column<int>(type: "int", nullable: false),
+                    is_default = table.Column<bool>(type: "bit", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    modified_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_address", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_address_address_address_id",
+                        column: x => x.address_id,
+                        principalTable: "address",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_user_address_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_address_country_id",
+                table: "address",
+                column: "country_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_address_address_id",
+                table: "user_address",
+                column: "address_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_address_user_id",
+                table: "user_address",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_address");
+
+            migrationBuilder.DropTable(
+                name: "address");
+
+            migrationBuilder.DropTable(
+                name: "country");
+        }
+    }
+}
