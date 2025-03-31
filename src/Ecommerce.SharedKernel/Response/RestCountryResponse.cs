570fb7b708e701d90e5ee3ef45f3eaa1b60815d3
Szxro <szxro@gmail.com>
2025-03-31 14:56:17 -0400

    Add rest country response class

---
+++ src/Ecommerce.SharedKernel/Response/RestCountryResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Ecommerce.SharedKernel.Response;
+
+public class RestCountryResponse
+{
+    public Name Name { get; set; } = null!;
+}
+
+public partial class Name
+{
+    public string Common { get; set; } = string.Empty;
+
+    public string Official { get; set; } = string.Empty;
+
+    public Dictionary<string, NativeName> NativeName { get; set; } = null!;
+}
+
+public class NativeName
+{
+    public string Official { get; set; } = string.Empty;
+
+    public string Common { get; set; } = string.Empty;
+}
