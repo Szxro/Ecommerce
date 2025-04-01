cb8d89b7ab67c8da324ed08ffbe452fb4d6fcd73
Szxro <szxro@gmail.com>
2025-04-01 11:36:50 -0400

    Remove domain event dispatcher service implementation and interface and move the logic to the worker

--- src/Ecommerce.Domain/Contracts/IDomainEventDispatcherService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ecommerce.SharedKernel.Common;
-using Ecommerce.SharedKernel.Contracts;
-
-namespace Ecommerce.Domain.Contracts;
-
-public interface IDomainEventDispatcherService
-{
-    Task PublishDomainEvent(IDomainEvent @event, CancellationToken cancellationToken = default);    
-}
