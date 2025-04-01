bdac53d21a621ab97fef2b466b7aadf6eaf74c95
Szxro <szxro@gmail.com>
2025-04-01 15:49:55 -0400

    Add command to reset the password and with its command validator

---
+++ src/Ecommerce.Application/Features/Users/Commands/ResetPasswordCommand/ResetPasswordCommand.cs
@@ -0,0 +1,84 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.SharedKernel.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.Errors;
+
+namespace Ecommerce.Application.Features.Users.Commands.ResetPasswordCommand;
+
+public record ResetPasswordCommand(string resetCode, string newPassword) : ICommand;
+
+public class ResetPasswordCommandHandler : ICommandHandler<ResetPasswordCommand>
+{
+    private readonly IEmailCodeRepository _emailCodeRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IHashService _hashService;
+    private readonly ICredentialsRepository _credentialsRepository;
+
+    public ResetPasswordCommandHandler(
+        IEmailCodeRepository emailCodeRepository,
+        IUnitOfWork unitOfWork,
+        IHashService hashService,
+        ICredentialsRepository credentialsRepository)
+    {
+        _emailCodeRepository = emailCodeRepository;
+        _unitOfWork = unitOfWork;
+        _hashService = hashService;
+        _credentialsRepository = credentialsRepository;
+    }
+    public async Task<Result> Handle(ResetPasswordCommand request, CancellationToken cancellationToken)
+    {
+        EmailCode? foundCode = await _emailCodeRepository.GetEmailCodeByEmailCodeAsync(request.resetCode, cancellationToken);
+
+        if (foundCode is null)
+        {
+            return Result.Failure(EmailCodeErrors.EmailCodeNotFound(request.resetCode));
+        }
+
+        Result result = ValidateEmailCode(foundCode);
+
+        if (result.IsFailure) return result;
+
+        foundCode.IsUsed = true;
+
+        _emailCodeRepository.Update(foundCode);
+
+        Credentials? foundCredentials = await _credentialsRepository.GetCredentialsByUsernameAsync(foundCode.User.Username.Value, cancellationToken);
+
+        if (foundCredentials is null)
+        {
+            return Result.Failure(CredentialsErrors.CredentialsNotFoundByUsername(foundCode.User.Username.Value));
+        }
+
+        foundCredentials.IsActive = false;
+
+        _credentialsRepository.Update(foundCredentials);
+
+        (string hash, byte[] salt) = _hashService.GetHashAndSalt(request.newPassword);
+
+        Credentials newCredentials = new Credentials
+        {
+            HashValue = hash,
+            SaltValue = Convert.ToHexString(salt),
+            IsActive = true,
+            User = foundCode.User
+        };
+
+        _unitOfWork.ChangeTrackerToUnchanged(foundCode.User);
+
+        _credentialsRepository.Add(newCredentials);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+
+    private static Result ValidateEmailCode(EmailCode emailCode)
+       => emailCode switch
+       {
+           { IsUsed: true } => Result.Failure(EmailCodeErrors.EmailCodeAlreadyUsed(emailCode.Code)),
+           { IsRevoked: true } => Result.Failure(EmailCodeErrors.EmailCodeAlreadyRevoked(emailCode.Code)),
+           { IsExpired: true } => Result.Failure(EmailCodeErrors.EmailCodeAlreadyExpired(emailCode.Code)),
+           _ => Result.Success(),
+       };
+}
---
+++ src/Ecommerce.Application/Features/Users/Commands/ResetPasswordCommand/ResetPasswordCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Ecommerce.Application.Features.Users.Commands.ResetPasswordCommand;
+
+public class ResetPasswordCommandValidator : AbstractValidator<ResetPasswordCommand>
+{
+    public ResetPasswordCommandValidator()
+    {
+        RuleFor(x => x.resetCode)
+            .NotEmpty().WithMessage("The reset code cant be empty.")
+            .NotNull().WithMessage("The reset code cant be null");
+
+        RuleFor(x => x.newPassword)
+            .NotEmpty().WithMessage("The reset code cant be empty.")
+            .NotNull().WithMessage("The reset code cant be null")
+            .MinimumLength(8).WithMessage("The password must be at least 8 characters long.")
+            .Matches(@"[A-Z]").WithMessage("The password must contain at least one uppercase letter.")
+            .Matches(@"[a-z]").WithMessage("The password must contain at least one lowercase letter.")
+            .Matches(@"[0-9]").WithMessage("The password must contain at least one number.")
+            .Matches(@"[\W_]").WithMessage("The password must contain at least one special character.");
+    }
+}
