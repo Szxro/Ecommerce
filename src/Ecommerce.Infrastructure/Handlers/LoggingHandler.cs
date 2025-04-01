9482704f647a6aa0d8b5e2580d04b55e7250a2e8
Sebastian <svargas@isf.do>
2025-03-31 20:19:11 -0400

    Add logging and retry http handlers. (delegate handlers)

---
+++ src/Ecommerce.Infrastructure/Handlers/LoggingHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Ecommerce.Infrastructure.Handlers;
+
+public class LoggingHandler : DelegatingHandler
+{
+    private readonly ILogger<LoggingHandler> _logger;
+
+    public LoggingHandler(ILogger<LoggingHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        try 
+        {
+            _logger.LogInformation("Sending request: {method} {url}", request.Method, request.RequestUri);
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken); 
+
+            response.EnsureSuccessStatusCode();
+
+            _logger.LogInformation("Received response: {statusCode} for {url}", response.StatusCode, request.RequestUri);
+
+            return response;
+
+        } catch (Exception ex)
+        {
+            _logger.LogError("An error occurred while sending request to {url} with the error message: {errorMessage}",
+                             request.RequestUri,
+                             ex.Message);
+            throw;
+        }
+    }
+}
---
+++ src/Ecommerce.Infrastructure/Handlers/RetryHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using Ecommerce.Domain.Contracts;
+
+namespace Ecommerce.Infrastructure.Handlers;
+
+public class RetryHandler : DelegatingHandler
+{
+    private readonly IExponentialBackoffService _exponentialBackoffService;
+
+    public RetryHandler(IExponentialBackoffService exponentialBackoffService)
+    {
+        _exponentialBackoffService = exponentialBackoffService;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        HttpResponseMessage? response = await _exponentialBackoffService.RetryWithBackoff(
+            () => base.SendAsync(request, cancellationToken),
+            cancellationToken: cancellationToken);
+
+        return response ?? throw new HttpRequestException("Request failed after all retries.");
+    }
+}
