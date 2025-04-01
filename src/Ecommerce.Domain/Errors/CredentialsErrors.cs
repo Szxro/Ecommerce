3e116d7cffa873f7a408b996cd588510dca7a237
Szxro <szxro@gmail.com>
2025-04-01 15:46:46 -0400

    Add credentials domain errorws

---
+++ src/Ecommerce.Domain/Errors/CredentialsErrors.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.SharedKernel.Common.Primitives;
+
+namespace Ecommerce.Domain.Errors;
+
+public static class CredentialsErrors
+{
+    public static Error CredentialsNotFoundByUsername(string username)
+        => Error.NotFound($"Credentials not found for the username '{username}'.");
+}
