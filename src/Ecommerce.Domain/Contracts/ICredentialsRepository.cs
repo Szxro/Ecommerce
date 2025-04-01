8c0886b524f6a14ed20f814060354095bd6a6d3a
Szxro <szxro@gmail.com>
2025-04-01 15:48:36 -0400

    Add credentials repository implementation and interface

---
+++ src/Ecommerce.Domain/Contracts/ICredentialsRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Domain.Contracts;
+
+public interface ICredentialsRepository 
+    : IRepositoryWriter<Credentials>
+{
+    Task<Credentials?> GetCredentialsByUsernameAsync(string username, CancellationToken cancellationToken = default);
+}
---
+++ src/Ecommerce.Infrastructure/Repositories/CredentialsRepository.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Infrastructure.Attributes;
+using Ecommerce.Infrastructure.Common;
+using Ecommerce.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Repositories;
+
+[Inject(ServiceLifetime.Scoped)]
+public class CredentialsRepository
+    : GenericRepository<Credentials>, ICredentialsRepository
+{
+    public CredentialsRepository(AppDbContext appDbContext) : base(appDbContext) { }
+
+    public async Task<Credentials?> GetCredentialsByUsernameAsync(string username, CancellationToken cancellationToken = default)
+    {
+        return await _appDbContext.Credentials
+                                  .Where(x => x.User.Username.Value == username && x.IsActive)
+                                  .FirstOrDefaultAsync(cancellationToken);
+    }
+}
