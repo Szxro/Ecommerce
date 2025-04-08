36472ed894a22c2181fbc3844393a4299a5f0d1f
Szxro <szxro@gmail.com>
2025-04-08 12:34:55 -0400

    Add product and product category domain errors

---
+++ src/Ecommerce.Domain/Errors/ProductCategoryErrors.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.SharedKernel.Common.Primitives;
+
+namespace Ecommerce.Domain.Errors;
+
+public static class ProductCategoryErrors
+{
+    public static Error ProductCategoryNotFound(string categoryName) 
+        => Error.NotFound($"The category '{categoryName}' was not found.");
+}
---
+++ src/Ecommerce.Domain/Errors/ProductErrors.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.SharedKernel.Common.Primitives;
+
+namespace Ecommerce.Domain.Errors;
+
+public static class ProductErrors
+{
+    public static Error ProductNameAlreadyRegistered
+        => Error.Conflict("Product name already registered.");
+}
