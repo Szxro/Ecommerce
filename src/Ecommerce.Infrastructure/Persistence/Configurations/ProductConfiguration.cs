95eb19fcb78f845a95c0ce292ff5e146cf118062
Szxro <szxro@gmail.com>
2025-04-08 10:52:57 -0400

    Add product entity configuration

---
+++ src/Ecommerce.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Persistence.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasIndex(x => x.Name).IsUnique();
+    }
+}
