c2982a84c4e81201702256397bf45eeb74bf60a9
Szxro <szxro@gmail.com>
2025-04-08 12:37:42 -0400

    Add missing configuration for the product category

---
+++ src/Ecommerce.Infrastructure/Persistence/Migrations/20250408162902_Product_Category_Migration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Product_Category_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "product_category",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_product_category_name",
+                table: "product_category",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_product_category_name",
+                table: "product_category");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "product_category",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
