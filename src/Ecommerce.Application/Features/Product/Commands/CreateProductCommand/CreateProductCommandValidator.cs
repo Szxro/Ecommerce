c60e3275dc30df5dd1e3cd3a890c8859beb2176a
Szxro <szxro@gmail.com>
2025-04-08 12:42:59 -0400

    Add validator for the create product command

---
+++ src/Ecommerce.Application/Features/Product/Commands/CreateProductCommand/CreateProductCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Ecommerce.Application.Features.Products.Commands.CreateProductCommand;
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.name)
+            .NotNull().WithMessage("The product name can't be null")
+            .NotEmpty().WithMessage("The product name can't be empty");
+
+        RuleFor(x => x.description)
+            .NotNull().WithMessage("The product description can't be null")
+            .NotEmpty().WithMessage("The product description can't be empty");
+
+        RuleFor(x => x.productCategory)
+            .NotNull().WithMessage("The product category can't be null")
+            .NotEmpty().WithMessage("The product category can't be empty");
+    }
+}
