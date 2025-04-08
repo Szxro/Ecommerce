36864e676ea460cfbf6e584cd2cf44fad66583a1
Szxro <szxro@gmail.com>
2025-04-08 12:37:57 -0400

    Add product controller

---
+++ src/Ecommerce.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Ecommerce.Application.Features.Products.Commands.CreateProductCommand;
+using Ecommerce.WebApi.Filters;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.Application.Common.DTOs.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Ecommerce.WebApi.Extensions;
+using Ecommerce.WebApi.Common;
+
+namespace Ecommerce.WebApi.Controllers;
+
+[Route("api/products")]
+[Authorize]
+[IsAdminFilter]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ProductController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<IResult> CreateProduct(CreateProductCommand createProduct)
+    {
+        Result<ProductDTO> result = await _sender.Send(createProduct);
+
+        return result.Match(
+            onSuccess: () => CustomResult.Success(result),
+            onFailure: CustomResult.Problem);
+    }    
+}
