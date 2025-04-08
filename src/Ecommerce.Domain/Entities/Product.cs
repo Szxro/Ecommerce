75f4f88defd3905fb965488c7ec8ac206228c643
Szxro <szxro@gmail.com>
2025-04-08 10:51:40 -0400

    Add product entity and product category entity

---
+++ src/Ecommerce.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.SharedKernel.Common;
+
+namespace Ecommerce.Domain.Entities;
+
+public class Product : Entity
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public int ProductCategoryId { get; set; }
+
+    public ProductCategory ProductCategory { get; set; } = null!;
+}
---
+++ src/Ecommerce.Domain/Entities/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.SharedKernel.Common;
+
+namespace Ecommerce.Domain.Entities;
+
+public class ProductCategory : Entity
+{
+    public ProductCategory()
+    {
+        Products = new HashSet<Product>();
+    }
+
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<Product> Products { get; set; }
+}
