bedbe6fcad8c588b4d61dd1056380daed3d4e0d5
Szxro <szxro@gmail.com>
2025-04-08 15:22:29 -0400

    Remove product unique name index

---
+++ src/Ecommerce.Infrastructure/Persistence/Migrations/20250408191640_Remove_Product_Index.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_Product_Index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_product_name",
+                table: "product");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "product",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "product",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_product_name",
+                table: "product",
+                column: "name",
+                unique: true);
+        }
+    }
+}
