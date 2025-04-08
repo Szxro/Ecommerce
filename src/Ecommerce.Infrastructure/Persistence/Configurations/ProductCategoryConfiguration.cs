2c08b35506089f04594250cd64cffee2d210c235
Szxro <szxro@gmail.com>
2025-04-08 12:29:48 -0400

    Add missing product category entity configuration

---
+++ src/Ecommerce.Infrastructure/Persistence/Configurations/ProductCategoryConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Persistence.Configurations;
+
+public class ProductCategoryConfiguration : IEntityTypeConfiguration<ProductCategory>
+{
+    public void Configure(EntityTypeBuilder<ProductCategory> builder)
+    {
+        builder.HasIndex(x => x.Name).IsUnique();
+    }
+}
