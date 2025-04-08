dca16beffd91089ef974713ff1a7c670b1e3c447
Szxro <szxro@gmail.com>
2025-04-08 13:14:08 -0400

    Add command to create a new product category and create a new controller for the product category

---
+++ src/Ecommerce.Application/Features/ProductCategory/Commands/CreateProductCategory/CreateProductCategoryCommand.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Domain.Errors;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.SharedKernel.Contracts;
+
+namespace Ecommerce.Application.Features.ProductCategories.Commands.CreateProductCategory;
+
+public record CreateProductCategoryCommand(string name):ICommand;
+
+public class CreateProductCategoryCommandHandler : ICommandHandler<CreateProductCategoryCommand>
+{
+    private readonly IProductCategoryRepository _categoryRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateProductCategoryCommandHandler(
+        IProductCategoryRepository categoryRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _categoryRepository = categoryRepository;
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<Result> Handle(CreateProductCategoryCommand request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(request.name) || string.IsNullOrWhiteSpace(request.name))
+        {
+            return Result.Failure(Error.Validation("The product category can't be null or empty or have white spaces"));
+        }
+
+        if (await _categoryRepository.IsProductCategoryNameNotUnique(request.name,cancellationToken))
+        {
+            return Result.Failure(ProductCategoryErrors.ProductCategoryAlreadyRegistered);
+        }
+
+        ProductCategory newCategory = new ProductCategory { Name = request.name };
+
+        _categoryRepository.Add(newCategory);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
---
+++ src/Ecommerce.WebApi/Controllers/ProductCategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.Application.Features.ProductCategories.Commands.CreateProductCategory;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.WebApi.Common;
+using Ecommerce.WebApi.Extensions;
+using Ecommerce.WebApi.Filters;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.WebApi.Controllers;
+
+[Route("api/product-category")]
+[Authorize]
+[IsAdminFilter]
+[ApiController]
+public class ProductCategoryController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ProductCategoryController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost("{category}")]
+    public async Task<IResult> CreateProductCategory(string category)
+    {
+        Result result = await _sender.Send(new CreateProductCategoryCommand(category));
+
+        return result.Match(
+            onSuccess: Results.Created,
+            onFailure: CustomResult.Problem);
+    }
+}
