39279ba51d423ad8113e9525ee275479611462af
Szxro <szxro@gmail.com>
2025-04-09 13:16:47 -0400

    Add product response dto

---
+++ src/Ecommerce.Application/Common/DTOs/Response/Queries/ProductResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecommerce.Application.Common.DTOs.Response.Queries;
+
+public class ProductResponse
+{
+    public string ProductName { get; set; } = string.Empty;
+
+    public string ProductDescription { get; set; } = string.Empty;
+
+    public int ProductPrice { get; set; }
+
+    public string ProductCategory { get; set; } = string.Empty;
+}
