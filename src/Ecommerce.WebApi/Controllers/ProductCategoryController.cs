c6638c01d6dfda4ab254b3432579fffce818aece
Szxro <szxro@gmail.com>
2025-04-14 16:39:21 -0400

    Add get, put and delete endpoint for the product category

--- src/Ecommerce.WebApi/Controllers/ProductCategoryController.cs
+++ src/Ecommerce.WebApi/Controllers/ProductCategoryController.cs
@@ -1,4 +1,8 @@
 ﻿using Ecommerce.Application.Features.ProductCategories.Commands.CreateProductCategory;
+using Ecommerce.Application.Features.ProductCategories.Commands.DeleteProductCategoryCommand;
+using Ecommerce.Application.Features.ProductCategories.Commands.UpdateProductCategory;
+using Ecommerce.Application.Features.ProductCategories.Queries.GetProductCategoriesQuery;
+using Ecommerce.Application.Common.DTOs.Response.Queries;
 using Ecommerce.SharedKernel.Common.Primitives;
 using Ecommerce.WebApi.Common;
 using Ecommerce.WebApi.Extensions;
@@ -22,6 +26,17 @@ public class ProductCategoryController : ControllerBase
         _sender = sender;
     }
 
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IResult> GetAllProductCategory()
+    {
+        Result<List<ProductCategoryResponse>> result = await _sender.Send(new GetProductCategoriesQuery());
+
+        return result.Match(
+            onSuccess: () => CustomResult.Success(result),
+            onFailure: CustomResult.Problem);
+    }
+
     [HttpPost("{category}")]
     public async Task<IResult> CreateProductCategory(string category)
     {
@@ -29,6 +44,26 @@ public class ProductCategoryController : ControllerBase
 
         return result.Match(
             onSuccess: Results.Created,
+            onFailure: CustomResult.Problem);
+    }
+
+    [HttpPut]
+    public async Task<IResult> UpdateProductCategory(UpdateProductCategoryCommand updateProduct)
+    {
+        Result result = await _sender.Send(updateProduct);
+
+        return result.Match(
+            onSuccess: Results.NoContent,
+            onFailure: CustomResult.Problem);
+    }
+
+    [HttpDelete("{categoryName}")]
+    public async Task<IResult> DeleteProductCategory(string categoryName)
+    {
+        Result result = await _sender.Send(new DeleteProductCategoryCommand(categoryName));
+
+        return result.Match(
+            onSuccess: Results.NoContent,
             onFailure: CustomResult.Problem);
     }
 }
