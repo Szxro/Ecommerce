e613f918e08ee790e38682b630123a034fdbe423
Szxro <szxro@gmail.com>
2025-04-14 16:34:36 -0400

    Change how the  == and != operator behave to not check the objecs by reference in memory but the value itself (like value objects)

--- src/Ecommerce.SharedKernel/Common/Primitives/Error.cs
+++ src/Ecommerce.SharedKernel/Common/Primitives/Error.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.SharedKernel.Enums;
+using System.Text.Json.Serialization;
 
 namespace Ecommerce.SharedKernel.Common.Primitives;
 
@@ -10,6 +11,7 @@ public class Error
 
     public ErrorType Type { get; }
 
+    [JsonConstructor]
     protected Error(
         string errorCode,
         string description,
@@ -19,6 +21,20 @@ public class Error
         Description = description;
         Type = type;
     }
+
+    // Note: need to override some operator to check not the reference in memory (when the error is deserialize it check if the reference are equal)
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Error other) return false;
+        // In this case is the values to check if the error are equal to another
+        return ErrorCode == other.ErrorCode && Description == other.Description && Type == other.Type;
+    }
+
+    public override int GetHashCode() => HashCode.Combine(ErrorCode, Description, Type); // Need to be implement
+
+    public static bool operator ==(Error a, Error b) => a?.Equals(b) ?? b is null;
+
+    public static bool operator !=(Error a, Error b) => !(a == b);
 
     public static Error None = new Error(string.Empty, string.Empty, ErrorType.None);
 
