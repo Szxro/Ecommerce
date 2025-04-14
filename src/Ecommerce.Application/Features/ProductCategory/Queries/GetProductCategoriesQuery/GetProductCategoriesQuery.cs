f5b7402f3e37a592a1f4e4e5bb0bbf1221465cb9
Szxro <szxro@gmail.com>
2025-04-14 16:38:42 -0400

    Add query logic to retrieve all the product categories

---
+++ src/Ecommerce.Application/Features/ProductCategory/Queries/GetProductCategoriesQuery/GetProductCategoriesQuery.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.Application.Common.Data;
+using Ecommerce.Application.Common.DTOs.Response.Queries;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.SharedKernel.Contracts;
+using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Application.Features.ProductCategories.Queries.GetProductCategoriesQuery;
+
+public record GetProductCategoriesQuery : ICachedQuery<List<ProductCategoryResponse>>
+{
+    public string CachedKey => "get-all-products-categories";
+}
+
+public class GetProductCategoryQueryHandler : IQueryHandler<GetProductCategoriesQuery, List<ProductCategoryResponse>>
+{
+    private readonly IApplicationDbContext _dbContext;
+
+    public GetProductCategoryQueryHandler(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Result<List<ProductCategoryResponse>>> Handle(GetProductCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        List<ProductCategoryResponse> categories = await _dbContext
+                                                                .ProductCategory
+                                                                .AsNoTracking()
+                                                                .Select(x => new ProductCategoryResponse(x.Name))
+                                                                .ToListAsync(cancellationToken);
+
+        return Result<List<ProductCategoryResponse>>.Success(categories);
+    }
+}
