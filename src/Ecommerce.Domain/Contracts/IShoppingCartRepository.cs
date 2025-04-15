c17846a56ede1602299cb68890ddcf24b064ad70
Szxro <szxro@gmail.com>
2025-04-15 14:45:49 -0400

    Add shopping cart repository implementation and interface

---
+++ src/Ecommerce.Domain/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Domain.Contracts;
+
+public interface IShoppingCartRepository 
+    : IRepositoryWriter<ShoppingCart>
+{
+    Task<ShoppingCart?> GetActiveShoppingCartsByUsernameAsync(string username, CancellationToken cancellationToken = default);
+}
---
+++ src/Ecommerce.Infrastructure/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Entities;
+using Ecommerce.Infrastructure.Attributes;
+using Ecommerce.Infrastructure.Common;
+using Ecommerce.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Repositories;
+
+[Inject(ServiceLifetime.Scoped)]
+public class ShoppingCartRepository
+    : GenericRepository<ShoppingCart>, IShoppingCartRepository
+{
+    public ShoppingCartRepository(AppDbContext appDbContext) : base(appDbContext) { }    
+
+    public async Task<ShoppingCart?> GetActiveShoppingCartsByUsernameAsync(string username, CancellationToken cancellationToken = default)
+    {
+        return await _appDbContext
+                        .ShoppingCart
+                        .Include(x => x.ShoppingCartProducts)
+                        .Where(x => x.User.Username.Value == username && !x.IsSubmit)
+                        .FirstOrDefaultAsync(cancellationToken);
+    }
+}
