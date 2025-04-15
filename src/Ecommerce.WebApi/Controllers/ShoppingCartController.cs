9c6c739284ff70e18109ee936920f5b1c80b4975
Szxro <szxro@gmail.com>
2025-04-15 14:46:30 -0400

    Add shopping cart controller

---
+++ src/Ecommerce.WebApi/Controllers/ShoppingCartController.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.Application.Features.ShoppingCarts.AddShoppingCartCommand;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.WebApi.Extensions;
+using Ecommerce.WebApi.Common;
+
+namespace Ecommerce.WebApi.Controllers;
+
+[Route("api/shopping-cart")]
+[Authorize]
+[ApiController]
+public class ShoppingCartController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ShoppingCartController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<IResult> AddToCart(AddShoppingCartCommand addShoppingCart)
+    {
+        Result result = await _sender.Send(addShoppingCart);
+
+        return result.Match(
+            onSuccess: Results.NoContent,
+            onFailure: CustomResult.Problem);
+    }
+}
