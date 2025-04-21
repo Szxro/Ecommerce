6d595c5d86360492b1c6495c3d4c36b3bec2e101
Szxro <szxro@gmail.com>
2025-04-21 13:10:57 -0400

    Add shopping cart dto and query to get the current active shopping cart

---
+++ src/Ecommerce.Application/Common/DTOs/Response/Queries/ShoppingCartDTO.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Application.Common.DTOs.Response.Queries;
+
+public class ShoppingCartDetailsDTO
+{
+    public string ProductName { get; set; } = string.Empty;
+
+    public int ProductPrice { get; set; }
+
+    public int Quantity { get; set; }
+}
+
+public class ShoppingCartDTO
+{
+    public List<ShoppingCartDetailsDTO> Details { get; set; } = null!;
+
+    public int TotalPrice { get; set; }
+}
---
+++ src/Ecommerce.Application/Features/ShoppingCart/Queries/GetCurrentCartQuery/GetCurrentCartQuery.cs
@@ -0,0 +1,62 @@
+﻿using Ecommerce.Application.Common.Data;
+using Ecommerce.Application.Common.DTOs.Response.Queries;
+using Ecommerce.Domain.Contracts;
+using Ecommerce.Domain.Errors;
+using Ecommerce.SharedKernel.Common.Primitives;
+using Ecommerce.SharedKernel.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Application.Features.ShoppingCarts.Queries.GetCurrentCartQuery;
+
+public record GetCurrentCartQuery : IQuery<ShoppingCartDTO>;
+
+public class GetCurrentCartQueryHandler : IQueryHandler<GetCurrentCartQuery,ShoppingCartDTO>
+{
+    private readonly IApplicationDbContext _applicationDbContext;
+    private readonly ICurrentUserService _currentUserService;
+
+    public GetCurrentCartQueryHandler(
+        IApplicationDbContext applicationDbContext,
+        ICurrentUserService currentUserService)
+    {
+        _applicationDbContext = applicationDbContext;
+        _currentUserService = currentUserService;
+    }
+
+    public async Task<Result<ShoppingCartDTO>> Handle(GetCurrentCartQuery request, CancellationToken cancellationToken)
+    {
+        string? currentUsername = _currentUserService.GetCurrentUserName();
+
+        if (string.IsNullOrEmpty(currentUsername) || string.IsNullOrWhiteSpace(currentUsername))
+        {
+            return Result<ShoppingCartDTO>.Failure(UserErrors.UsernameNotUniqueOrInvalid);
+        }
+
+        ShoppingCartDTO? currentCart = await _applicationDbContext.ShoppingCart
+                                                     .AsNoTracking()
+                                                     .Include(x => x.ShoppingCartDetails)
+                                                     .ThenInclude(x => x.Product)
+                                                     .Where(x => x.User.Username.Value == currentUsername && !x.IsSubmit && x.TotalPrice > 0)
+                                                     .Select(x => new ShoppingCartDTO
+                                                     {
+                                                         Details = x.ShoppingCartDetails
+                                                                    .Where(x => !x.IsRemoved && x.Quantity > 0)
+                                                                    .Select(x => new ShoppingCartDetailsDTO
+                                                                    {
+                                                                        ProductName = x.Product.Name,
+                                                                        ProductPrice = x.Product.Price,
+                                                                        Quantity = x.Quantity
+                                                                    })
+                                                                    .ToList(),
+                                                         TotalPrice = x.TotalPrice
+                                                     })
+                                                     .FirstOrDefaultAsync(cancellationToken);
+
+        if (currentCart is null)
+        {
+            return Result<ShoppingCartDTO>.Failure(ShoppingCartErrors.ShoppingCartNotFoundByUsername);
+        }
+
+        return Result<ShoppingCartDTO>.Success(currentCart);
+    }
+}
