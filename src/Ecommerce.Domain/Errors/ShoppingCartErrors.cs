5c19a292c3646dadcd835455c1a235f90c40cd77
Szxro <szxro@gmail.com>
2025-04-21 13:09:28 -0400

    Add shopping cart errors

---
+++ src/Ecommerce.Domain/Errors/ShoppingCartErrors.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.SharedKernel.Common.Primitives;
+
+namespace Ecommerce.Domain.Errors;
+
+public static class ShoppingCartErrors
+{
+    public static Error ShoppingCartNotFoundByUsername
+        => Error.NotFound("No Active shopping cart were found by the given username, check and try again.");
+}
