190eda2606fd34cbba60ab2a978d18a01e01194b
Szxro <szxro@gmail.com>
2025-04-21 13:12:53 -0400

    Add delete and get endpoints with their respective commands

--- src/Ecommerce.WebApi/Controllers/ShoppingCartController.cs
+++ src/Ecommerce.WebApi/Controllers/ShoppingCartController.cs
@@ -1,10 +1,13 @@
-﻿using Ecommerce.Application.Features.ShoppingCarts.AddShoppingCartCommand;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Ecommerce.SharedKernel.Common.Primitives;
 using Ecommerce.WebApi.Extensions;
 using Ecommerce.WebApi.Common;
+using Ecommerce.Application.Features.ShoppingCarts.Commands.AddShoppingCartCommand;
+using Ecommerce.Application.Features.ShoppingCarts.Commands.RemoveCartProductCommand;
+using Ecommerce.Application.Features.ShoppingCarts.Queries.GetCurrentCartQuery;
+using Ecommerce.Application.Common.DTOs.Response.Queries;
 
 namespace Ecommerce.WebApi.Controllers;
 
@@ -21,12 +24,33 @@ public class ShoppingCartController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<IResult> AddToCart(AddShoppingCartCommand addShoppingCart)
+    public async Task<IResult> AddToCart(AddCartProductCommand addShoppingCart)
     {
         Result result = await _sender.Send(addShoppingCart);
 
         return result.Match(
             onSuccess: Results.NoContent,
+            onFailure: CustomResult.Problem);
+    }
+
+    [HttpDelete]
+
+    public async Task<IResult> RemoveFromCart(RemoveCartProductCommand removeCart)
+    {
+        Result result = await _sender.Send(removeCart);
+
+        return result.Match(
+            onSuccess: Results.NoContent,
+            onFailure: CustomResult.Problem);
+    }
+
+    [HttpGet]
+    public async Task<IResult> GetCart()
+    {
+        Result<ShoppingCartDTO> result = await _sender.Send(new GetCurrentCartQuery());
+
+        return result.Match(
+            onSuccess: () => CustomResult.Success(result),
             onFailure: CustomResult.Problem);
     }
 }
